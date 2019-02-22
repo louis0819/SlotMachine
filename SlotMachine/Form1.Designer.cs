0d4e7511dc4d3d599f7d33a63f88e0cab80019c6
Louisli <louis1995201@gmail.com>
2019-02-22 13:14:39 +0800

    practice

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.picBtn = new System.Windows.Forms.PictureBox();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.nudQty = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(131, 153);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(88, 88);
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(231, 153);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(88, 88);
+            this.pic2.TabIndex = 0;
+            this.pic2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(331, 153);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(88, 88);
+            this.pic3.TabIndex = 0;
+            this.pic3.TabStop = false;
+            // 
+            // picBtn
+            // 
+            this.picBtn.Location = new System.Drawing.Point(473, 51);
+            this.picBtn.Name = "picBtn";
+            this.picBtn.Size = new System.Drawing.Size(106, 353);
+            this.picBtn.TabIndex = 1;
+            this.picBtn.TabStop = false;
+            this.picBtn.Click += new System.EventHandler(this.picBtn_Click);
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.BackColor = System.Drawing.Color.Transparent;
+            this.lblSum.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblSum.Location = new System.Drawing.Point(142, 365);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(54, 20);
+            this.lblSum.TabIndex = 2;
+            this.lblSum.Text = "label1";
+            // 
+            // nudQty
+            // 
+            this.nudQty.BackColor = System.Drawing.Color.Linen;
+            this.nudQty.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.nudQty.Location = new System.Drawing.Point(416, 361);
+            this.nudQty.Name = "nudQty";
+            this.nudQty.Size = new System.Drawing.Size(61, 29);
+            this.nudQty.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(81, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "總數量";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(356, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "投注量";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Linen;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cherry 5X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Linen;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(12, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Star 10X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Linen;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(12, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Watermelon 15X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Linen;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(12, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Bar 20X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::SlotMachine.Properties.Resources.backing;
+            this.ClientSize = new System.Drawing.Size(564, 392);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudQty);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.picBtn);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.Text = "SlotMachine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox picBtn;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.NumericUpDown nudQty;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
